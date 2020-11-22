a4fc8f11195e8bbb307befbf5b980ecc70c471d4
Sam <sweatersjpg@gmail.com>
2020-11-22 12:39:35 -0500

    Changed link back to plain URI encoding so links work in twitter

--- runner.js
+++ runner.js
@@ -1,13 +1,14 @@
 // p5 runner
 // sweatersjpg
 
-let chars = '<>#%"{}|\\^[]`;/?:@&=+&$, \n\xa0';
-let index = 'àáâäæãåāèéêëēėęûüùúūîïíīįì';
+// let chars = '<>#%"{}|\\^[]`;/?:@&=+&$, \n\xa0';
+// let index = 'àáâäæãåāèéêëēėęûüùúūîïíīįì';
 
 // get perameters
 const urlParams = new URLSearchParams(window.location.search);
 if(urlParams.has('c')) { // if code perameter exists
-  let code = revert(urlParams.get('c'));
+  // let code = revert(urlParams.get('c'));
+  let code = decodeURIComponent(urlParams.get('c'));
   var script = document.createElement('script'); // create script element
   script.innerHTML = code; // add the code
   document.getElementsByTagName('head')[0].appendChild(script); // attach it to head
@@ -19,28 +20,28 @@ if(urlParams.has('c')) { // if code perameter exists
 }
 
 function ConvertInput() {
-  let input = document.getElementById('codeInput');
-  let encoded = convert(input.value);
-  encoded = encoded.replace(" ","");
-  let output = document.getElementById('output');
-  output.innerHTML = "Your new link"
-  output.href = window.location.href + "?c=" + encoded;
+  let input = document.getElementById('codeInput'); // get input
+  // let encoded = convert(input.value);
+  let encoded = encodeURIComponent(input.value); // encode input
+  let output = document.getElementById('output'); // get link
+  output.innerHTML = "Your new link" // change link name
+  output.href = window.location.href + "?c=" + encoded; // change link destination
 }
 
-function convert(str) {
-  let a = str.split('');
-  for (var i = 0; i < a.length; i++) {
-    if(!chars.includes(a[i])) continue;
-    a[i] = index.charAt(chars.indexOf(a[i]));
-  }
-  return a.join("");
-}
-
-function revert(str) {
-  let a = str.split('');
-  for (var i = 0; i < a.length; i++) {
-    if(!index.includes(a[i])) continue;
-    a[i] = chars.charAt(index.indexOf(a[i]));
-  }
-  return a.join("");
-}
+// function convert(str) {
+//   let a = str.split('');
+//   for (var i = 0; i < a.length; i++) {
+//     if(!chars.includes(a[i])) continue;
+//     a[i] = index.charAt(chars.indexOf(a[i]));
+//   }
+//   return a.join("");
+// }
+//
+// function revert(str) {
+//   let a = str.split('');
+//   for (var i = 0; i < a.length; i++) {
+//     if(!index.includes(a[i])) continue;
+//     a[i] = chars.charAt(index.indexOf(a[i]));
+//   }
+//   return a.join("");
+// }
